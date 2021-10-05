e3406211b29714f61967cd2075e998242c685aef
waldomagalona <waldomagalona@gmail.com>
2021-10-05 19:22:24 +0800

    add files

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  # root "articles#index"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/articles', to: 'articles#index', as: 'articles'
+  get '/articles/new', to: 'articles#new', as: 'new_article'
+  post '/articles', to: 'articles#create'
+  get '/articles/:id', to: 'articles#show', as: 'article'
+  get '/articles/:id/edit', to: 'articles#edit', as: 'edit_article'
+  patch '/articles/:id', to: 'articles#update'
+  delete '/articles/:id', to: 'articles#destroy'
 end
